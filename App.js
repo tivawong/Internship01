76be7f663ffc1f1bc39b6a74c94e789f481e4d00
tivaiice <tiva.wk@hotmail.com>
2020-04-21 11:11:45 +0700

    commit 1

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+
+  const [outputText, setOutputTExt] = useState(initialState)
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Change Text" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
